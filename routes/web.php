6cfa5247c7b58691c789030b1a3c11505dace589
Jelte ten Holt <inotherwords@gmail.com>
2022-05-24 10:00:08 -0600

    changed text dashboard route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use TheRiptide\LaravelDynamicText\Http\Livewire\TextIndex;
 use TheRiptide\LaravelDynamicText\Middleware\AuthorizeDashboardMiddleware;
 
-Route::get('dashboard/texts', TextIndex::class )->name('dashboard_texts')->middleware(['web', AuthorizeDashboardMiddleware::class]);
+Route::get('dashboard/texts/edit', TextIndex::class )->name('dashboard_texts')->middleware(['web', AuthorizeDashboardMiddleware::class]);
